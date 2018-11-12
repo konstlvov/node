04f3fba30662651a78995ae994cf65d2237d20d8
Konstantin Lvov <k.lvov@abr.ru>
2018-11-12 16:31:33 +0300

    parsing requests paths

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 ﻿var http = require('http');
+var url = require('url');
+
 var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url);
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+\/+$/g, ''); // wtf?
+  console.log('you have requested path:' + path);
+  console.log('you have requested trimmed path:' + trimmedPath);
   res.end('Hello world');
 });
 
