792cf50031dcd9f4515b0802c24d9006ded94d85
Konstantin Lvov <k.lvov@abr.ru>
2018-11-12 16:12:06 +0300

    minimal server (without express)

---
+++ index.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+var server = http.createServer(function (req, res) {
+  res.end('Hello world');
+});
+
+server.listen(3000, function() {
+  console.log('listening on port 3000...');
+});
