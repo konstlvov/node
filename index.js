d5fa37eeff0596548aac70a98749c55c3a79f03d
Konstantin Lvov <k.lvov@abr.ru>
2018-11-27 16:32:40 +0300

    Разбор тела запроса (request body). Запрос обрабатываеся как поток (stream), поэтому обработчик req.on('data'...) может вызываться много раз. В обработчике req.on('end'...) можно отдать ответ (вместо того, чтобы отдавать его сразу внутри функции-параметра для http.createServer)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 ﻿var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 var server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true); // true tells node to convert query string to Object
@@ -7,16 +8,31 @@ var server = http.createServer(function (req, res) {
   var trimmedPath = path.replace(/^\/+\/+$/g, ''); // wtf?
   console.log('you have requested path:' + path);
   console.log('you have requested trimmed path:' + trimmedPath);
+  //
   var method = req.method; // typeof req.method is string
   console.log('Requested method is: ' + method); // GET
+  //
   var headers = req.headers;
   //console.log('Request headers:\n' + JSON.stringify(headers));
   console.log('Request headers:', headers); // more pretty printed in console
   console.log('Requested host: ' + headers.host);
+  //
   var queryStringObject = parsedUrl.query;
-  console.log(queryStringObject);
-  console.log(JSON.stringify(queryStringObject));
-  res.end('Hello world привет');
+  console.log('queryStringObject: ', queryStringObject);
+  //
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  //
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  //
+  req.on('end', function() {
+    buffer += decoder.end();
+    // moving res.end into the handler of 'end' event of req object
+    res.end('Hello world привет');
+    console.log('Request received with payload: ', buffer);
+  });
 });
 
 server.listen(3000, function() {
