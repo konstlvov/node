baeaa7de13662d9f66032b3b9007ca8a12c3e07a
Konstantin Lvov <k.lvov@abr.ru>
2019-04-22 17:01:12 +0300

    Пример использования технологии socket.io (вебсокеты) для создания чата, который работает в браузере

---
+++ adhoc-samples/socket-chat-example/index.js
@@ -0,0 +1,22 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+  socket.on('chat message', function(msg){
+    console.log('about to io.emit message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+      console.log('user disconnected');
+  });  
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
