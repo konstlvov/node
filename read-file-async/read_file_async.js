bb5e9918d8507967a9de47566b3fecb9605a31bc
Konstantin Lvov <k.lvov@abr.ru>
2019-04-22 17:02:44 +0300

    Пример синхронного и асинхронного чтения файла для отдачи его в браузер. Асинхронное чтение показывает лучшую производительность.

---
+++ read-file-async/read_file_async.js
@@ -0,0 +1,8 @@
+﻿// начинает передавать файл сразу, не дожидаясь, пока он будет полностью прочитан в память
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream(__dirname + '/data.txt')
+  stream.pipe(res)
+})
+server.listen(3000)
