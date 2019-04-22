0aefaba15a044649cf32562bc844f7dccc078f26
Konstantin Lvov <k.lvov@abr.ru>
2019-04-22 13:00:14 +0300

    Попробую удалить и создать index.js в корне

--- pirple/index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.end('Hello world');
-});
-
-server.listen(3000, function() {
-  console.log('listening on port 3000...');
-});
